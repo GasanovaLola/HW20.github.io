d613afa5df22e6e6a4503a4409f63823695e031a
Lola Gasanova <63504067+GasanovaLola@users.noreply.github.com>
2020-12-13 19:23:57 +0200

    commit new

--- main.js
+++ main.js
@@ -2,28 +2,22 @@ let table = document.getElementById("table");
 let buttonSave;
 let buttonCancel;
 let text;
+let newElem;
 
 table.onclick = function (e) {
-    console.log(e.target.tagName, "e");
-    if (e.target.tagName == "TD") {
-        console.log("click on td");
-        text = e.target.querySelector(".text");
-        text.className = "text2";
-
-        buttonSave = document.createElement("button");
-        buttonCancel = document.createElement("button");
-
-        buttonSave.innerHTML = "save";
-        buttonCancel.innerHTML = "cancel";
-
-        buttonSave.className = "buttonSave";
-        buttonCancel.className = "buttonCancel";
-
-        e.target.append(buttonSave);
-        e.target.append(buttonCancel);
+    elem = e.target;
+    console.log(elem);
+    if (elem.tagName == "TD") {
+        if (!elem.querySelector(".text2") || !elem.querySelector(".buttonSave")) {
+            CreateClassForText(elem);
+            CreateButtons(elem);
+        }
 
         buttonSave.onclick = function() {
             console.log("save");
+
+            buttonSave.remove();
+            buttonCancel.remove();
         }
 
         buttonCancel.onclick = function() {
@@ -31,6 +25,31 @@ table.onclick = function (e) {
 
             buttonSave.remove();
             buttonCancel.remove();
+
+            newElem = document.createElement("p");
+            newElem.className = "text";
+            newElem.innerText = "some text"
+            
+            elem.append(newElem);
         }
     }
+}
+
+function CreateButtons(elem) {
+    buttonSave = document.createElement("button");
+    buttonCancel = document.createElement("button");
+
+    buttonSave.innerHTML = "save";
+    buttonCancel.innerHTML = "cancel";
+
+    buttonSave.className = "buttonSave";
+    buttonCancel.className = "buttonCancel";
+
+    elem.append(buttonSave);
+    elem.append(buttonCancel);
+}
+
+function CreateClassForText(elem) {
+    text = elem.querySelector(".text");
+    text.className = "text2";
 }
